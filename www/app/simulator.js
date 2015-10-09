d96124ccd9432eb5c4376bb06f0eebf83f8bde59
Cris Fuhrman <fuhrmanator+git@gmail.com>
2015-10-09 08:57:23 -0400

    onclick not working

--- www/app/simulator.js
+++ www/app/simulator.js
@@ -1,13 +1,25 @@
-﻿define(["./Hero","./Weapon"], function (Hero, Weapon) {
-    return {
-        start: function () {
-            console.log('Starting simulation');
-            var h1 = new Hero("Joe", 12, Weapon.DAGGER);
-            var h2 = new Hero("Ellen", 14, Weapon.TWO_HANDED_SWORD);
+﻿define(["./heroes", "./Hero", "./Weapon"], function (heroes, Hero, Weapon) {
+  return {
+    getSelectValues: function(selectElement) {
+      var result = [];
+      var options = selectElement && selectElement.options;
+      var opt;
 
-            console.log(h1.getName() + " " + h1.getST() + " " + h1.getWeapon());
-            console.log(h2.getName() + " " + h2.getST() + " " + h2.getWeapon());
+      for (var i = 0, iLen = options.length; i < iLen; i++) {
+        opt = options[i];
 
+        if (opt.selected) {
+          result.push(opt.value || opt.text);
         }
-    };
+      }
+      return result;
+    },
+    start: function () {
+      console.log('Starting simulation');
+      var selectElement = document.getElementById("heroesSelected");
+      console.log(selectElement);
+      var selectedHeroes = this.getSelectValues(selectElement);
+      alert(selectedHeroes);
+    }
+  };
 });
